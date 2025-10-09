428616d8e2c9b97dfa9d43c22a74d9d6f46749e5
Giovanne Isaac Marques <giovanneim020804@gmail.com>
2025-10-09 00:55:46 +0000

    ExcluindoPastaNext

--- dundermifflin/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/xFM/JOPgw+E915kKj1mDnM3Uzc5+KrOHIDIfQ6ccyA=\"\n}"
--- dundermifflin/.next/static/chunks/src_app_layout_jsx_0a548d63._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__1fd63f60._.css",
-  "static/chunks/node_modules_next_dist_c2ad5bda._.js"
-],
-    source: "dynamic"
-});
