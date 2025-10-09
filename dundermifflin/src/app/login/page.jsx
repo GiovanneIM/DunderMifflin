b0be9ab22169db5340a39c46d4754053a6daf274
Giovanne Isaac Marques <giovanneim020804@gmail.com>
2025-10-09 00:11:33 +0000

    NovaLogoEAcertoDoFooter

--- dundermifflin/src/app/login/page.jsx
+++ dundermifflin/src/app/login/page.jsx
@@ -1,1 +1,35 @@
-export default function Login () {}
+import './login.css'
+import Image from 'next/image'
+
+export default function Login() {
+    return <>
+        <div className='corpo d-flex flex-wrap'>
+            <div className='container'>
+<div>
+                <Image
+                    src='/img/DUNDERbranco.svg'
+                    alt='Logo Dunner Mifflin'
+                    width={200}
+                    height={500}
+                />
+            </div>
+
+            <div className='div_branca'>
+                teste
+            </div><div>
+                <Image
+                    src='/img/DUNDERbranco.svg'
+                    alt='Logo Dunner Mifflin'
+                    width={200}
+                    height={500}
+                />
+            </div>
+
+            <div className='div_branca'>
+                teste
+            </div>
+            </div>
+            
+        </div>
+    </>
+}
