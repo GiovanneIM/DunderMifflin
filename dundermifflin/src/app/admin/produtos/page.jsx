8a75c4b798d81bb0c54ee1189151a06b711f4fb3
Giovanne Isaac Marques <giovanneim020804@gmail.com>
2025-10-21 20:00:25 +0000

    modalsETC

--- dundermifflin/src/app/admin/produtos/page.jsx
+++ dundermifflin/src/app/admin/produtos/page.jsx
@@ -1,3 +1,4 @@
+
 import CardAdmin from "@/components/CardAdmin";
 
 export default async function Produtos() {
@@ -6,11 +7,13 @@ export default async function Produtos() {
 
     return <>
         <div className="container corpo py-4">
-            <h2 className="titulo fs-2 text-center mb-4">Registrar produto</h2>
+            <h2 className="titulo fs-2 text-center mb-4">Produtos</h2>
 
-            {produtos && produtos.map((produto, index) => {
-                return <CardAdmin produto={produto} key={index}/>
-            })}
+            <div className="d-flex flex-wrap gap-3 justify-content-center">
+                {produtos && produtos.map((produto, index) => {
+                    return <CardAdmin produto={produto} key={index} />
+                })}
+            </div>
         </div>
     </>
 }
--- dundermifflin/src/components/CardAdmin/index.jsx
+++ dundermifflin/src/components/CardAdmin/index.jsx
@@ -1,31 +1,59 @@
-"use client"
+'use client'
 
-import { useState, useEffect } from "react";
 import './card.css'
 
-export default function CardAdmin ({ produto }) {
+export default function CardAdmin({ produto }) {
 
-
+    // Modal
+    function abrirModal() {
+        const modal = new bootstrap.Modal(document.getElementById('modalEstocar'))
+        modal.show()
+    }
 
     return <>
-        <div className="card" style={{ width: "15rem" }}>
-            <div className="card-img">
-                <img src={produto.imagem[0]} alt={produto.nome} />
+        <div className='ca rounded fundoBranco'>
+            <div className='ca-ID fundoPreto rounded-bottom-0'>ID {produto.id}</div>
+
+            <div className='ca-img'>
+                <img src={produto.imagem[0]} />
             </div>
 
-            <div className="card-corpo">
-                <div className="card-nome">
-                    {produto.nome}
-                </div>
+            <div className='ca-infos col-10 rounded-top-0'>
+                <div>{produto.nome}</div>
+                <div>{produto.marca}</div>
 
-                <div className="card-preco">
-                    R$ {produto.preco.toFixed(2).replace('.', ',')}
-                </div>
+                <div>Em estoque: <span>{produto.quantidade}</span></div>
 
-                <div className='card-botoes'>
-                    <a href={''} className="btn btn-1">
-                        Atualizar Produto
-                    </a>
+                <div>{produto.preco.toFixed(2).replace('.', ',')}</div>
+            </div>
+
+            <div className="ca-botoes">
+                <a href={`/admin/produtos/produto/${produto.id}`}><button className='btn btn-1'>Ver Produto</button></a>
+                <button className='btn btn-2' onClick={abrirModal}>Estocar</button>
+            </div>
+        </div>
+
+        {/* MODAL */}
+        <div className="modal fade" id="modalEstocar" tabIndex="-1" aria-labelledby="modalImagemLabel" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+
+                    {/* CABEÇALHO */}
+                    <div className="modal-header fundoPreto">
+                        <h5 className="modal-title" id="modalImagemLabel">Adicionar Imagem</h5>
+                        <button type="button" className="btn-close btn-fechar" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                    </div>
+
+                    {/* INPUT */}
+                    <div className="modal-body">
+
+                    </div>
+
+                    {/* BOTÕES */}
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" >Cancelar</button>
+                        <button type="button" className="btn btn-primary" >Adicionar</button>
+                    </div>
                 </div>
             </div>
         </div>
